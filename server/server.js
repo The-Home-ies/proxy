1f4b8b648cdde0d8224146f8d77669da03370225
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2020-12-17 11:03:52 -0800

    added loader.io file and get request

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const router = require('./router');
 
 const app = express();
 
+app.get('/loaderio-bd6b1017b0ce147ad819196d28dcc904', (req, res) => {
+    res.sendFile(path.join(__dirname, 'loaderio-bd6b1017b0ce147ad819196d28dcc904.txt'));
+});
+
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, '..', 'public')));
 
