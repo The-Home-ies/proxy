41816872f6dd17475531b261e861e1660ba7340c
meekoder <meekorusdi@gmail.com>
2020-12-17 19:29:17 -0800

    jasons nginx url

--- server/config/services.js
+++ server/config/services.js
@@ -1,31 +1,11 @@
 module.exports = {
-  // photoCarousel: {
-  //   api: '/photo-carousel/:id/photos',
-  //   url: 'http://localhost:3001',
-  //   bundle: '/photo-carousel/:id/bundle.js',
-  // },
-  // reservations: {
-  //   api: '/listings/:id',
-  //   url: 'http://localhost:3002',
-  //   bundle: '/listings/:id/build/bundle.js',
-  // },
-  // reviews: {
-  //   api: '/review-listings/:id/reviews',
-  //   url: 'http://localhost:3003',
-  //   bundle: '/:id/bundle.js',
-  // },
-  // placesToStay: {
-  //   api: '/rooms/:id/places',
-  //   url: 'http://localhost:3004',
-  //   bundle: '/rooms/:id/main.js',
-  // },
   photoCarousel: {
-    api: '/',
+    api: '/photo-carousel/:id/photos',
     url: 'http://54.241.52.191:3001',
     bundle: '/:id/bundle.js',
   },
   reservations: {
-    api: '/:id',
+    api: '/listings/:id',
     url: 'http://54.151.102.122',
     bundle: '/:id/build/bundle.js',
   },
@@ -36,7 +16,7 @@ module.exports = {
   },
   placesToStay: {
     api: '/:id/places',
-    url: 'http://18.144.8.147:3004',
+    url: 'http://54.177.32.2',
     bundle: '/:id/main.js',
   },
 };
